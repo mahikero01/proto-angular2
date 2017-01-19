a0301f23a24b6907295c7a717eb46c65d4e3f05f
mahikero <federicoparassarmiento@yahoo.com>
2017-01-19 14:21:43 -0500

    up

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular2/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: ``
+    template: `
+        <h4>Todos List</h4>
+        <h5>Number of Todos: <span class="badge">{{todos.length}}</span></h5>
+        <ul class="list-group">
+            <li *ngFor="let todo of todos" class="list-group-item">
+                {{todo}}
+            </li>
+        </ul>
+        <div class="form-inline">
+            <input class="form-control" #todotext>
+            <button class="btn btn-default" (click)="addTodo(todotext.value)">
+                Add Todo
+            </button>
+        </div>
+    `
 })
 
 export class AppComponent {
+    todos: Array<string>;
+    
     constructor() {
-        
+        this.todos = ["Todo 1", "Todo 2", "Todo 3"];
+    }
+
+    addTodo(todo: string) {
+        this.todos.push(todo);
     }
 }
